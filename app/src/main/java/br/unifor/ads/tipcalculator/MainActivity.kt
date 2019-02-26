24b1ef13874cc14d72d1d2a8b5324b6f2c3c78d7
Lucas Viana <lucasvddt2@gmail.com>
2019-02-26 20:38:11 -0300

    Adicionando funcionalidades

--- app/src/main/java/br/unifor/ads/tipcalculator/MainActivity.kt
+++ app/src/main/java/br/unifor/ads/tipcalculator/MainActivity.kt
@@ -6,6 +6,8 @@ import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import android.text.Editable
+
 
 class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
@@ -18,40 +20,58 @@ class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
     private lateinit var mTextViewVariablePercentage: TextView
     private lateinit var mTextViewVariablePercentageValue: TextView
     private lateinit var mTextViewVariablePercentageResult: TextView
-
+    var billValue: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var perc = "%"
+        mSeekBarPercentage = findViewById(R.id.main_seekbar_percentage)
+        mSeekBarPercentage.setOnSeekBarChangeListener(this)
+
+
+        mTextViewFixedPercentage = findViewById(R.id.main_edittext_fixed_percentage)
+        val fixed_percentage_string = 10.toString()
+        mTextViewFixedPercentage.setText("$fixed_percentage_string$perc")
+
+
+        mTextViewVariablePercentage = findViewById(R.id.main_edittext_variable_percentage)
+        mTextViewVariablePercentageValue = findViewById(R.id.main_edittext_variable_percentage_value)
+        mTextViewVariablePercentageResult = findViewById(R.id.main_edittext_percentage_variable_result)
+
+
         mEditTextBill = findViewById(R.id.main_edittext_bill)
         mEditTextBill.setOnEditorActionListener { view, actionId, event ->
 
             if (actionId == EditorInfo.IME_ACTION_DONE){
-                val billValue = mEditTextBill.text
-                mTextViewFixedPercentageResult.text = billValue
+                val billValue = mEditTextBill.text.toString().toDouble()
+                var aux = casas(billValue);
+                val auxString = aux.toString()
+                val cifrao="R$"
+                val billValueConcat = "$cifrao $auxString"
+                mEditTextBill.setText(billValueConcat);
+
+
+                mTextViewFixedPercentageValue = findViewById(R.id.main_edittext_fixed_percentage_value)
+                mTextViewFixedPercentageValue.text = minTipValue(billValue).toString()
+                mTextViewFixedPercentageResult = findViewById(R.id.main_edittext_fixed_percentage_result)
+                mTextViewFixedPercentageResult.text = minTipResult(billValue).toString()
+                mTextViewFixedPercentageResult.text = billValueConcat
             }
 
             false
         }
 
 
-        mSeekBarPercentage = findViewById(R.id.main_seekbar_percentage)
-        mSeekBarPercentage.setOnSeekBarChangeListener(this)
-
-
-        mTextViewFixedPercentage = findViewById(R.id.main_edittext_fixed_percentage)
-        mTextViewFixedPercentageValue = findViewById(R.id.main_edittext_fixed_percentage_value)
-        mTextViewFixedPercentageResult = findViewById(R.id.main_edittext_fixed_percentage_result)
-        mTextViewVariablePercentage = findViewById(R.id.main_edittext_variable_percentage)
-        mTextViewVariablePercentageValue = findViewById(R.id.main_edittext_variable_percentage_value)
-        mTextViewVariablePercentageResult = findViewById(R.id.main_edittext_percentage_variable_result)
 
     }
 
     override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-        mTextViewVariablePercentage.text = progress.toString()
+        mTextViewVariablePercentage.text = progress.toString()+"%"
+        mTextViewVariablePercentageValue.text = "R$"+ (billValue.toString().toDouble()*(progress.toDouble()/100))
+        mTextViewVariablePercentageResult.text = "R$" + (billValue.toString().toDouble()+ billValue.toString().toDouble()*(progress.toDouble()/100))
     }
 
     override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -60,6 +80,18 @@ class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
     override fun onStopTrackingTouch(seekBar: SeekBar?) {
 
+    }
+    fun casas(v:Double ):Double{
+        val value = v/100
+        return value
+    }
+    fun minTipValue(v:Double):Double{
+        val minTip = v/10
+        return minTip
+    }
+    fun minTipResult(v:Double):Double{
+        val Tip = v/10 + v
+        return Tip
     }
 
 }
